536550582085dd0694619b97c5f3d1c81c4926ba
Samuel Colak <sam@samcolak.com>
2026-04-27 08:57:15 +0200

    update for geoposition utility for micro speed optimizations

--- src/geoutils.rs
+++ src/geoutils.rs
@@ -1,6 +1,9 @@
 
 
 
+const DEG_TO_RAD: f64 = std::f64::consts::PI / 180.0;
+
+
 #[derive(Clone, Debug)]
 pub struct GeoPosition {
     pub longitude: f64,
@@ -11,10 +14,10 @@ pub struct GeoPosition {
 
 pub fn geo_distance_between(long1: f64, lat1: f64, long2: f64, lat2: f64) -> f64 {
 
-    let _dlon = (long2 - long1) * 0.0174532925;
-    let _dlat = (lat2 - lat1) * 0.0174532925;
-    let _lat1 = lat1 * 0.0174532925;
-    let _lat2 = lat2 * 0.0174532925;
+    let _dlon = (long2 - long1) * DEG_TO_RAD;
+    let _dlat = (lat2 - lat1) * DEG_TO_RAD;
+    let _lat1 = lat1 * DEG_TO_RAD;
+    let _lat2 = lat2 * DEG_TO_RAD;
 
     let _a = f64::sin(_dlat/2.0) * f64::sin(_dlat/2.0) + f64::sin(_dlon/2.0) * f64::sin(_dlon/2.0) * f64::cos(_lat1) * f64::cos(_lat2);
     
@@ -33,6 +36,23 @@ impl GeoPosition {
             latitude
         }
 
+    }
+
+    pub fn distanceto_within_box(&self, longitude: f64, latitude: f64, offset: f64) -> f64 
+    {        
+        let _dlon = (self.longitude - longitude);
+        let _dlat = (self.latitude - latitude);
+        
+        if _dlon.abs() > offset || _dlat.abs() > offset {
+            return -1.0;
+        }
+
+        geo_distance_between(
+            self.longitude, 
+            self.latitude, 
+            longitude, 
+            latitude
+        )
     }
 
     pub fn distanceto(&self, longitude: f64, latitude: f64) -> f64 
